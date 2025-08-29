a737a5b7521770bf752ac718172539f2f24e342c
Bharati-Dhere <dbharati5162@gmail.com>
2025-08-29 06:14:35 +0530

    feedback email

--- src/utils/sendAdminReply.js
+++ src/utils/sendAdminReply.js
@@ -1,8 +1,9 @@
 // Utility to call backend email API
 export async function sendAdminReply({ to, subject, text, html }) {
-  const res = await fetch('/api/send-email', {
+  const res = await fetch('https://production-project-1.onrender.com/api/send-email', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
     body: JSON.stringify({ to, subject, text, html })
   });
   if (!res.ok) throw new Error('Failed to send email');
